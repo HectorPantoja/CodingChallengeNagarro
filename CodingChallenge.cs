da74230e0121657199caf163ca3a83dd540a183e
hector.pantoja <hector.pantoja@techops.mx>
2021-06-30 01:11:34 -0500

    Changes

--- CodingChallenge.cs
+++ CodingChallenge.cs
@@ -1,17 +1,54 @@
 ﻿using CodingChallenge;
 using System;
+using System.Globalization;
+//Simple coding exercise:
+ 
+//In the programming language of your choice, write a program that parses a sentence and replaces each word with the following: first letter, number of distinct characters between first and last character, and last letter.
+//Words are separated by spaces or non-alphabetic characters and these separators should be maintained in their original form and location in the answer. 
+
+//For example:
+
+//Input 1:                It was many and many a year ago
+//Output 1:            I0t w1s m2y a1d m2y a y2r a1o
+
+//Input 2:                Copyright, Microsoft:Corporation
+//Output 2:            C7t, M6t:C7n
+
+//User interface expectations :
+//Input: Command Line Interface: One should be able to pass the argument (input) on command line
+//Output: Should print the output on command line
+
+
+//Please follow the rules below:
+ 
+//•	Code variables must be in English.
+//•	Use the main function arguments.
+//•	The function should not be static nor main.
+//•	The function should not use hardcoded values.
+//•	The function must be reusable.
+//•	Document your code.
+//•	Provide the file(s) in .zip format.
+
+
+
+//A few of the things we will be looking at is accuracy, efficiency, solution completeness. 
+
+//Please include this problem description in the comment at the top of your solution. 
+//The problem is designed to take approximately 1-2 hours and will be used as a conversation point in the verbal assessment part of the on-site interview.  Please complete the coding assessment and have your solution uploaded as soon as possible.Please upload your solution to Dropbox or similar option (not G-Drive) and send a link to download the folder prior to your interview. [NOTE: Do include the results of your test run with your test data, along with the source-code while mailing us your solution]
 
 namespace CodingChallegeNagarro
 {
-    class CodingChallenge
+    static class CodingChallenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ac43rd0".ParseLetters());
-        }
-        public string Parse(string str)
-        {
-            return str.ParseLetters();
+            ParsingClass obj = new ParsingClass(); //Initialize a new object of parsing class
+
+            //for loop to get the output from obj.ParseLetters
+            for(int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"Output { i } : { obj.ParseLetters(args[i]) }");
+            }
         }
     }
 }
--- ParsingClass.cs
+++ ParsingClass.cs
@@ -8,23 +8,23 @@ using System.Text.RegularExpressions;
 
 namespace CodingChallenge
 {
-    public static class ParsingExtensionMethod
+    public class ParsingClass
     {
-        public static string ParseLetters(this string str){
+        public string ParseLetters(string str){
             StringBuilder stringBuilder = new StringBuilder();//to build the string that will be return
             StringBuilder tmp = new StringBuilder();//temporal to catch string between special characters or numbers
-            var OnlyLetters = new Regex("^[a-zA-Z]+$");//Regex to identify when its a character
+            var regexCharacters = new Regex("^[a-zA-Z]+$");//Regex to identify when its a character
             
             
             for(int i = 0; i < str.Length; i++)
             {
-                var flag = OnlyLetters.Match(str[i].ToString());// boolean flag that triggers where append count of letters and special characters
+                var flag = regexCharacters.Match(str[i].ToString());// boolean flag that triggers where append count of letters and special characters
 
                 if (!flag.Success)
                 {
                     if(tmp.Length > 0)
                     {
-                        stringBuilder.Append(GetDistincts(tmp));
+                        stringBuilder.Append(GetDistincts(tmp)); // get the count of different characters
                         tmp.Clear();
                     }
                     stringBuilder.Append(str[i]);
@@ -41,9 +41,12 @@ namespace CodingChallenge
             return stringBuilder.ToString();
 
         }
-        private static string GetDistincts(StringBuilder tmp)
+        private string GetDistincts(StringBuilder tmp)
         {
-            int distinct = tmp.ToString().Substring(1, tmp.Length - 2).Distinct().Count();
+            if (tmp.Length == 1)// if length of tmp is 1 that means is just a letter which it'll keep the same position in the original text
+                return tmp.ToString();
+
+            int distinct = tmp.ToString().Substring(1, tmp.Length - 2).Distinct().Count(); // get the count of different characters
             
             return string.Concat(tmp[0], distinct, tmp[tmp.Length - 1]);
         }
